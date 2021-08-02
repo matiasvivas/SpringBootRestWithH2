bac1f09e0cc566389e35951608c80bbf10f357d9
Matias A Vivas <matiasvivas@hotmail.com>
2021-08-02 15:48:34 -0300

    Create data-h2.sql
    Insert de 9 superheroes de Avengers de ejemplo.

---
+++ src/main/resources/data-h2.sql
@@ -0,0 +1,9 @@
+INSERT INTO SUPERHEROE (id, name) VALUES (1, 'Ironman');
+INSERT INTO SUPERHEROE (id, name) VALUES (2, 'Spiderman');
+INSERT INTO SUPERHEROE (id, name) VALUES (3, 'Black Panter');
+INSERT INTO SUPERHEROE (id, name) VALUES (4, 'American Captain');
+INSERT INTO SUPERHEROE (id, name) VALUES (5, 'Thor');
+INSERT INTO SUPERHEROE (id, name) VALUES (6, 'Loki');
+INSERT INTO SUPERHEROE (id, name) VALUES (7, 'Antman');
+INSERT INTO SUPERHEROE (id, name) VALUES (8, 'Hulk');
+INSERT INTO SUPERHEROE (id, name) VALUES (9, 'Black Widow');
